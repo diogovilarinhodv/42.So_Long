04663b3051d31ae3cb13b76c5ec6644ed046bd25
digui13 <www.eee.az@gmail.com>
2021-09-27 12:59:48 +0100

    Add files via upload

--- src/so_long.c
+++ src/so_long.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpestana <dpestana@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/27 10:38:30 by dpestana          #+#    #+#             */
+/*   Updated: 2021/09/27 12:11:38 by dpestana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "so_long.h"
 
 int	key_hook(int key_press, t_game *g)
@@ -22,12 +34,14 @@ int	main(int argc, char **argv)
 	g.count = inic_counter();
 	g.map = render_map();
 	if (argc != 2)
+		return (args_error());
+	if (read_map_file(argv[argc - 1], &g))
 		return (1);
-	read_map_file(argv[argc - 1], &g);
 	g.win = render_window(g);
 	g.textures = render_textures(g);
-	printing_textures(&g);
-	mlx_hook(g.win.mlx_win, KEY_PRESS, KEY_PRESS_MASK, key_hook, &g);
+	if (printing_textures(&g) == 1)
+		return (1);
+	mlx_hook(g.win.mlx_win, KEY_PRESS, KEY_PRESS_MASK << 0, key_hook, &g);
 	mlx_string_put(g.win.mlx, g.win.mlx_win, 0, 0, 255, "Moves: 0");
 	mlx_loop(g.win.mlx);
 	game_over(&g);
--- src/so_long.h
+++ src/so_long.h
@@ -1,11 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpestana <dpestana@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/27 10:38:48 by dpestana          #+#    #+#             */
+/*   Updated: 2021/09/27 12:40:42 by dpestana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include "../mlx/mlx.h"
+# include <mlx.h>
 # include "../gnl/get_next_line.h"
 # include <fcntl.h>
 
-# define KEY_PRESS_MASK 1L<<0
+# define KEY_PRESS_MASK 1L
 # define KEY_PRESS 02
 
 # define X_BUTTON_EXIT 17
@@ -37,7 +49,7 @@ typedef struct s_textures {
 typedef struct s_player {
 	int	x;
 	int	y;
-	int moves;
+	int	moves;
 
 }	t_player;
 
@@ -45,10 +57,10 @@ typedef struct s_counter
 {
 	int	player;
 	int	exit;
-	int collect;
-	int enemy;
-	int wall;
-	int floor;
+	int	collect;
+	int	enemy;
+	int	wall;
+	int	floor;
 
 }	t_counter;
 
@@ -70,18 +82,26 @@ typedef struct s_game
 
 }	t_game;
 
-t_counter	inic_counter();
-t_map 		render_map();
+t_counter	inic_counter(void);
+t_map		render_map(void);
 t_window	render_window(t_game g);
-t_textures	render_textures();
+t_textures	render_textures(t_game g);
 
-int		read_map_file(char *map_path, t_game *g);
-int		printing_textures(t_game *g);
-void	move(t_game *g, int inc_x, int inc_y);
-void	game_over(t_game *g);
+int			read_map_file(char *map_path, t_game *g);
+int			printing_textures(t_game *g);
+void		move(t_game *g, int inc_x, int inc_y);
+int			game_over(t_game *g);
 
-size_t	ft_strlen(const char *s);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_itoa(int n);
+size_t		ft_strlen(const char *s);
+char		*ft_strjoin(char const *s1, char const *s2);
+char		*ft_itoa(int n);
+
+int			args_error(void);
+int			alloc_mem_error(int *fd);
+int			open_file_error(int *fd);
+int			square_error(t_game *g);
+int			map_char_invalid(t_game *g);
+int			too_much_entities(t_game *g);
+int			wall_error(t_game *g, int y, int x);
 
 #endif
