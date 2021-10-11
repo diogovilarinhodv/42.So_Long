f3604f350eb54db394aedec3fc77e3aac3e2c8d7
digui13 <www.eee.az@gmail.com>
2021-10-11 16:03:03 +0100

    Add files via upload

--- utils/error_handling/error_reading.c
+++ utils/error_handling/error_reading.c
@@ -6,7 +6,7 @@
 /*   By: dpestana <dpestana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/27 12:53:07 by dpestana          #+#    #+#             */
-/*   Updated: 2021/10/09 18:08:58 by dpestana         ###   ########.fr       */
+/*   Updated: 2021/10/11 15:03:36 by dpestana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,14 @@ void	error_alloc_mem(int *fd, t_game *g)
 		game_over(g, 0, 1, 1);
 	else
 		game_over(g, 0, 0, 1);
+}
+
+// [reading.c]
+void	error_line_longer(t_game *g)
+{
+	write(1, "Error\n", 7);
+	write(1, "Line longer than others\n", 25);
+	game_over(g, 0, 1, 1);
 }
 
 // [reading.c]
